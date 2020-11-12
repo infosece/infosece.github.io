7ee2c33fa6b17abc4e8321cbe4f597172536ac95
davizardb <davi.luis@aluno.uece.br>
2020-11-12 20:23:27 -0300

    Template Uploaded

---
+++ js/dz.carousel.js
@@ -0,0 +1,79 @@
+/* JavaScript Document */
+jQuery(document).ready(function() {
+    'use strict';
+
+	
+	/*  Blog post Carousel function by = owl.carousel.js */
+	jQuery('.blog-carousel').owlCarousel({
+		loop:true,
+		autoplay:true,
+		margin:30,
+		nav:false,
+		dots: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive:{
+			0:{
+				items:1
+			},
+			480:{
+				items:2
+			},			
+			991:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
+	})
+	
+	/*  Blog post Carousel function by = owl.carousel.js */
+	jQuery('.course').owlCarousel({
+		loop:true,
+		autoplay:true,
+		margin:30,
+		nav:false,
+		dots: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive:{
+			0:{
+				items:1
+			},
+			480:{
+				items:2
+			},			
+			991:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
+	})
+	
+	/* testimonial two function by = owl.carousel.js */
+	jQuery('.testimonial-two').owlCarousel({
+		loop:true,
+		margin:30,
+		nav:false,
+		dots: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive:{
+			0:{
+				items:1
+			},
+			
+			480:{
+				items:2
+			},			
+			
+			991:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
+	})
+});
+/* Document .ready END */
